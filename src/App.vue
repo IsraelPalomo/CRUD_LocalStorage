6ee6d0662a78b53b6c06220d79ae4f6ca8a51e3d
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-11-13 11:32:22 +0100

    fondo animado agregado

--- src/App.vue
+++ src/App.vue
@@ -20,5 +20,21 @@ a {
 }
 body {
 	overflow-x: hidden;
+
+	background: linear-gradient(139deg, #cd96d3, #13bbb2, #ab003c, #87b819, #6d26f4);
+	background-size: 300% 300%;
+	animation: gradient-animation 40s ease infinite;
+
+	@keyframes gradient-animation {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
 }
 </style>
